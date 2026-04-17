4479d99b53d397a8d1d23841efef2b046f90ad5b
sujith kumar <sujith.kumar@tudip.com>
2026-04-17 13:43:26 +0530

    Trigger CI/CD pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
  
 app.get('/', (req, res) => { 
-  res.json({ message: 'Hello h hi from CI/CD Pipeline!', version: '1.0' }); 
+  res.json({ message: 'Hello hi from CI/CD Pipeline!', version: '1.0' }); 
 }); 
  
 app.listen(PORT, () => { 
