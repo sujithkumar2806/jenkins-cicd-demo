8fd9bcea18620b5e3e74a8da119250390c017b3b
sujith kumar <sujith.kumar@tudip.com>
2026-04-17 15:07:40 +0530

    autobuild

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hi jsut a yping pratcoec y',
+    message: 'Hi bro my jenkin is working properly  ',
     version: '1.0'
   });
 });
