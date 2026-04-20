7b2979b4e28762f6fd894ed97f9d05a726bbc9c3
sujith kumar <sujith.kumar@tudip.com>
2026-04-20 17:28:54 +0530

    add name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'This  is  Sujith Mulammagari',
+    message: 'This  is  Sujith Mulammagari from tudip',
     version: '1.0'
   });
 });
